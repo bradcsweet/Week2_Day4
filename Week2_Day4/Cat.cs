47c80e4c82934bf498fd18dfee0b25d662e58cf1
bradcsweet <bradcsweet@gmail.com>
2017-10-05 15:14:01 -0400

    Intro to OOP

---
+++ Week2_Day4/Cat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day4
+{
+    class Cat
+    {
+        //STATES
+        //fields
+        private string name;
+
+        //Properties (c# specific)
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+            // decides if you get access to fields, the private category 
+           
+        }
+        
+            
+        //BEHAVIORS
+
+
+        //CONSTRUCTORS
+
+        public Cat()
+        {
+            //default construction because it takes no 
+            //parameters in the parentheses
+        }
+
+    }
+}
