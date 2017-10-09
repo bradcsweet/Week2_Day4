0048e8f8b774cd51db99cd92b5238544d223d9af
bradcsweet <bradcsweet@gmail.com>
2017-10-09 13:47:28 -0400

    dog display with beep

--- Week2_Day4/Cat.cs
+++ Week2_Day4/Cat.cs
@@ -8,62 +8,62 @@ namespace Week2_Day4
 {
     class Cat
     {
-        //STATES
-        //fields
-        private string name;
-        private int age;
-        private string furColor;
-        private bool isHungry = true; //we can set the values of variables in fields
+        ////STATES
+        ////fields
+        //private string name;
+        //private int age;
+        //private string furColor;
+        //private bool isHungry = true; //we can set the values of variables in fields
 
 
-        //Properties (c# specific)
-        public string Name
-        {
-            get { return this.name; } //this going to the program showing us what we want to see
-            set { this.name = value; } //this is us changing the value from the program
-            // decides if you get access to fields, the private category 
-        }
+        ////Properties (c# specific)
+        //public string Name
+        //{
+        //    get { return this.name; } //this going to the program showing us what we want to see
+        //    set { this.name = value; } //this is us changing the value from the program
+        //    // decides if you get access to fields, the private category 
+        //}
         
             
-        //BEHAVIORS
+        ////BEHAVIORS
 
 
-        //CONSTRUCTORS
+        ////CONSTRUCTORS
 
-        public Cat()
-        {
-            //default constructor because it takes no 
-            //parameters in the parentheses
-        }
+        //public Cat()
+        //{
+        //    //default constructor because it takes no 
+        //    //parameters in the parentheses
+        //}
 
-        public Cat(string name, int age, string furColor) //overloading a constructor. assigning values right away.
-        {
-            this.name = name;
-            this.age = age;                 //Constructor is a type of method!
-            this.furColor = furColor;
-            //field         parameter
-        }
+        //public Cat(string name, int age, string furColor) //overloading a constructor. assigning values right away.
+        //{
+        //    this.name = name;
+        //    this.age = age;                 //Constructor is a type of method!
+        //    this.furColor = furColor;
+        //    //field         parameter
+        //}
 
-        //Methods
+        ////Methods
 
-        public void Eat()
-        {
-            if(isHungry) // same as if(isHungry == true)
-            {
-                isHungry = false;
-            }
-            Console.WriteLine("Is the cat hungry? " + isHungry);
+        //public void Eat()
+        //{
+        //    if(isHungry) // same as if(isHungry == true)
+        //    {
+        //        isHungry = false;
+        //    }
+        //    Console.WriteLine("Is the cat hungry? " + isHungry);
 
-        }
+        //}
 
-        public void WorkOut()
-        {
-            if (isHungry == false)
-            {
-                isHungry = true;
-            }
-            Console.WriteLine("The cat worked out. Hungry now? " + isHungry);
-        }
+        //public void WorkOut()
+        //{
+        //    if (isHungry == false)
+        //    {
+        //        isHungry = true;
+        //    }
+        //    Console.WriteLine("The cat worked out. Hungry now? " + isHungry);
+        //}
 
     }
 }
--- Week2_Day4/Program.cs
+++ Week2_Day4/Program.cs
@@ -10,18 +10,33 @@ namespace Week2_Day4
     {
         static void Main(string[] args)
         {
-            Cat mittens = new Cat();
+            //Cat mittens = new Cat();
 
-            mittens.Name = "Mittens";
+            //mittens.Name = "Mittens";
 
-            Cat secondCat = new Cat("Oz", 3, "orange and white");
+            //Cat secondCat = new Cat("Oz", 3, "orange and white");
 
-            Console.WriteLine(mittens.Name);
-            Console.WriteLine(secondCat.Name);
+            //Console.WriteLine(mittens.Name);
+            //Console.WriteLine(secondCat.Name);
 
-            mittens.Eat();
-            mittens.WorkOut();
+            //mittens.Eat();
+            //mittens.WorkOut();
 
+            Dog firstDoge = new Dog();
+
+            firstDoge.Weight = 60.00;
+            firstDoge.Height = 3.2f;
+            firstDoge.FurLength = "flowing mane";
+            firstDoge.RunningSpeed = "will fetch ... eventually";
+
+            firstDoge.Groom();
+            Console.WriteLine(firstDoge.FurLength);
+
+            firstDoge.Run();
+            Console.WriteLine("The dog now weighs " + firstDoge.Weight);
+            Console.WriteLine(firstDoge.RunningSpeed);
+
+            firstDoge.Bark();
         }
     }
 }
